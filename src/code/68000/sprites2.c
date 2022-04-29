2cb8b2642ab58542b46f30216f013630487ffa4c
Fabien Sanglard <fabien.sanglard@gmail.com>
2022-04-29 11:04:21 -0700

    DONE

--- src/code/68000/sprites2.c
+++ src/code/68000/sprites2.c
@@ -2,5 +2,5 @@
 0b00000000_00100000  X Flip
 0b00000000_01000000  Y Flip
 0b00000000_10000000  Unused
-0b00001111_00000000  X block size (in sprites)
-0b11110000_00000000  Y block size (in sprites)
+0b00001111_00000000  X sprite size (in tiles)
+0b11110000_00000000  Y sprite size (in tiles)
