1580b967f7e96cd09fe5e61a6bc457ff707007ec
Fabien Sanglard <fabien.sanglard@gmail.com>
2022-05-15 22:43:25 -0700

    Capcom Sound Team

---
+++ src/code/okistruct.c
@@ -0,0 +1,4 @@
+typedef struct {
+	uint8_t firstOffset[3];
+	uint8_t lastOffset[3];
+} oky_entry;
--- src/code/z80/music.c
+++ src/code/z80/music.c
@@ -1,18 +1,18 @@
-int8_t pauseCounter = 0;
+int8_t noopCounter = 0;
 
 void updateMusic() {
-  if (pauseCounter) {
-  	pauseCounter--;
-  	return;
+  if (noopCounter) {
+    noopCounter--;
+    return;
   }
 
   int8_t bc = next();
   switch (bc) {
-    case XX : pauseCounter = bc; break;
+    case XX : noopCounter = bc; break;
     case YY : 
-  	   REG_YM2151_CMD = next(); 
-  	   REG_YM2151_DAT = next(); 
-  	   break;
+       REG_YM2151_CMD = next(); 
+       REG_YM2151_DAT = next(); 
+       break;
     case ZZ : 
        REG_OKI = 0x8 | next();
        REG_OKI = next();
