4ebc8ad69d0a05eedd09393d0b9d775d4b122273
Fabien Sanglard <fabien.sanglard@gmail.com>
2022-07-30 15:27:09 -0700

    Proofreading p178

--- src/code/68000/init_vars.c
+++ src/code/68000/init_vars.c
@@ -1,15 +1,19 @@
-// There are defined by the linker via the linker script
-extern char _etext, _data, _edata, _bss, _ebss;
+// These are defined by the linker via the linker script
+extern char _etext _data, _edata, _bss, _ebss;
 
 char *src = &_etext;
 char *dst = &_data;
 
 // Copy ROM to DATA
-while (dst < &_edata) {
-  *dst++ = *src++;
+void copyDATA() {
+  while (dst < &_edata) {
+    *dst++ = *src++;
+  }
 }
 
 // Zero BSS.
-for (dst = &_bss; dst< &_ebss; dst++) {
-  *dst = 0;
+void clearBSS() {
+  for (dst = &_bss; dst< &_ebss; dst++) {
+    *dst = 0;
+  }
 }
--- src/code/68000/memory_map.c
+++ src/code/68000/memory_map.c
@@ -1,13 +1,10 @@
+#define ALIGN(X) __attribute__ ((aligned (X)))
 #define GFXRAM __attribute__ ((section (".gfx_data")))
 #define CPSA   __attribute__ ((section (".cpsa_reg")))  
 #define CPSB   __attribute__ ((section (".cpsb_reg")))  
-// LATCHA TODO
-// LATCHb TODO
-// JAMMA  TODO
+// ... All other SECTIONS here.
 
-GFXRAM short palettes[6 * 32 * 16];
+GFXRAM ALIGN(256) short palettes[6 * 32 * 16];
 CPSA   short cpsa_reg[0x20] = {};
 CPSB   short cpsb_reg[0x20] = {};
-// LATCHA TODO
-// LATCHb TODO
-// JAMMA  TODO
+// ... All memory mapped data structures here.
