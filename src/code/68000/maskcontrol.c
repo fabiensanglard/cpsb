757866e61e0f3020dce64e1dfda95f2b4c801df3
Fabien Sanglard <fabien.sanglard@gmail.com>
2022-08-24 09:16:17 -0700

    Fix priority groups 16-bitfield

--- src/code/68000/maskcontrol.c
+++ src/code/68000/maskcontrol.c
@@ -1,4 +1,4 @@
-3333222211110000 - Priority 4 pens SCROLL tile group1
-7777666655554444 - Priority 4 pens SCROLL tile group2
-BBBBAAAA99998888 - Priority 4 pens SCROLL tile group3
-FFFFEEEEDDDDCCCC - Priority 4 pens SCROLL tile group4
+FEDCBA9876543210 - Priority Mask bitfield group0
+FEDCBA9876543210 - Priority Mask bitfield group1
+FEDCBA9876543210 - Priority Mask bitfield group2
+FEDCBA9876543210 - Priority Mask bitfield group3
